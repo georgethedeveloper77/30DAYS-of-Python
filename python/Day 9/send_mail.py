bf868c9fadf069ea3bf365f1a7f4288f7d819bff
george karani <[georgekarani77@gmail.com]>
2020-05-31 21:05:04 +0300

    Send Email & Read Inbox

--- python/Day 9/send_mail.py
+++ python/Day 9/send_mail.py
@@ -1,26 +1,34 @@
 import smtplib
-from email.mine.text import MIMEText
+from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
 
-username ='moodsync0@gmail.com'
-password='ihate@mypassword#'
+username = 'moodsync0@gmail.com'
+password = 'ihate@mypassword#'
 
 
-def send_mail(text='Email Body', subject='Hello World', from_email= 'moodsync0 <moodsync0@gmail.com>', to_emails=None):
-  assert isinstance(to_emails, list)
-  msg = MIMEMultipart('alternative')
-  msg['From'] = from_email
-  msg['To'] = ", "
+def send_mail(text='Email Body', subject='Hello World', from_email='moodsync0 <moodsync0@gmail.com>', to_emails=None, html=None):
+    assert isinstance(to_emails, list)
+    msg = MIMEMultipart('alternative')
+    msg['From'] = from_email
+    msg['To'] = ", ".join(to_emails)
+    msg['Subject'] = subject
 
-  msg_str=""
+    txt_part = MIMEText(text, 'plain')
+    msg.attach(txt_part)
+    # msg.attach(txt_part)
+    if html != None:
+      html_part = MIMEText("<h1>This is working</h1>", 'html')
+      msg.attach(html_part)
+      #msg.attach(html_part)
+    msg_str = msg.as_string()
     # login to my smtp server
-  server = smtplib.SMTP(host='smtp.gmail.com', port=587)
-  server.ehlo()
-  server.starttls()
-  server.login(username, password)
-  server.send(from_email, to_emails, msg_str)
-  server.quit()
-    #with smtplib.SMTP() as server:
+    server = smtplib.SMTP(host='smtp.gmail.com', port=587)
+    server.ehlo()
+    server.starttls()
+    server.login(username, password)
+    server.sendmail(from_email, to_emails, msg_str)
+    server.quit()
+    # with smtplib.SMTP() as server:
     # server.login(username, password)
     # pass
